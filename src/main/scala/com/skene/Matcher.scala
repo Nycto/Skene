c6378da76ad5949093cd897851b240964ed2bf0e
Nycto <James@RoundEights.com>
2011-09-18 09:21:05 -0700

    Added basic Matchers

---
+++ src/main/scala/com/skene/Matcher.scala
@@ -0,0 +1,81 @@
+package main.scala.com.skene {
+
+    import main.scala.com.skene.matcher._
+
+    /**
+     * Helper methods creating matchers
+     */
+    object Matcher {
+        def path ( path: String ) = new Path(path)
+        val always = new Always
+        val never = new Never
+    }
+
+    /**
+     * A construct for matching patterns against a context
+     */
+    trait Matcher {
+        /**
+         * Returns whether a given context matches
+         */
+        def matches ( context: Context ): Boolean
+    }
+}
+
+package main.scala.com.skene.matcher {
+
+    import main.scala.com.skene.Context
+    import main.scala.com.skene.Matcher
+
+    /**
+     * Matches against the path of a request
+     */
+    class Path ( path: String ) extends Matcher {
+        /**
+         * The path being compared against
+         */
+        private val versus = if ( path.startsWith("/") ) path else "/" + path
+
+        /**
+         * @see Matcher
+         */
+        override def matches ( context: Context )
+            = context.url.path.getOrElse("/") == versus
+
+        /**
+         * Create a readable description of this matcher
+         */
+        override def toString () = "[Path Matcher: " + versus + "]"
+    }
+
+    /**
+     * A matcher that always passes
+     */
+    class Always extends Matcher {
+        /**
+         * @see Matcher
+         */
+        override def matches ( context: Context ) = true
+
+        /**
+         * Create a readable description of this matcher
+         */
+        override def toString () = "[Always Matcher]"
+    }
+
+    /**
+     * A matcher that never passes
+     */
+    class Never extends Matcher {
+        /**
+         * @see Matcher
+         */
+        override def matches ( context: Context ) = false
+
+        /**
+         * Create a readable description of this matcher
+         */
+        override def toString () = "[Never Matcher]"
+    }
+}
+
---
+++ src/test/scala/com/skene/MatcherTest.scala
@@ -0,0 +1,40 @@
+package test.scala.com.skene.matcher
+
+import org.specs2.mutable._
+import org.specs2.mock.Mockito
+
+import main.scala.com.skene._
+
+class MatcherTest extends Specification with Mockito {
+
+    "A Path matcher" should {
+
+        val context = mock[Context];
+        context.url returns URL("http://example.com/path/to/resource")
+
+        "match a full path" in {
+            Matcher.path("/path/to/resource").matches(context) must_== true
+        }
+
+        "not match a different path" in {
+            Matcher.path("/some/other/path").matches(context) must_== false
+        }
+
+        "not care about a leading slash" in {
+            Matcher.path("path/to/resource").matches(context) must_== true
+        }
+    }
+
+    "An Always matcher" should {
+        "always return true" in {
+            Matcher.always.matches( mock[Context] ) must_== true
+        }
+    }
+
+    "A Never matcher" should {
+        "always return false" in {
+            Matcher.never.matches( mock[Context] ) must_== false
+        }
+    }
+}
+
