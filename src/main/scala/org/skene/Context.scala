1709ed1493c62a1b6ff27c8ae284be007247163d
Nycto <James@RoundEights.com>
2011-10-09 12:04:00 -0700

    Added ContextDecorator and ParameterizedContext

--- src/main/scala/org/skene/Context.scala
+++ src/main/scala/org/skene/Context.scala
@@ -1,14 +1,63 @@
 package org.skene
 
 /**
- * Represents the context of a request and the associated response
+ * The data associated with a request
  */
 trait Context {
 
     /**
      * The requested URL
      */
-    def url: URL;
+    def url: URL
+
+    /**
+     * Any parameters associated with the request
+     */
+    def params: Map[String, String] = Map()
+
+    /**
+     * Returns a version of this context with new parameters added
+     */
+    def withParams ( newParams: Map[String, String] ): Context = {
+        newParams.nonEmpty match {
+            case true => new ParameterizedContext( this, params ++ newParams )
+            case false => this
+        }
+    }
+
+    def withParams( newParams: (String, String)* ): Context
+        = withParams( newParams.toMap )
 }
 
+/**
+ * A context that wraps another context and updates parts of it
+ */
+abstract class ContextDecorator
+    ( private val inner: Context )
+    extends Context
+{
+
+    /**
+     * @see Context
+     */
+    override def url: URL = inner.url
+
+    /**
+     * @see Context
+     */
+    override def params: Map[String, String] = inner.params
+}
+
+/**
+ * A context wrapper that adds parameters to an existing context
+ */
+class ParameterizedContext
+    ( inner: Context, newParams: Map[String, String] )
+    extends ContextDecorator( inner )
+{
+    /**
+     * @see Context
+     */
+    override val params: Map[String, String] = newParams
+}
 
---
+++ src/test/scala/org/skene/ContextTest.scala
@@ -0,0 +1,25 @@
+package test.scala.com.skene
+
+import org.specs2.mutable._
+import org.specs2.mock._
+
+import org.skene._
+
+class ParameterizedContextTest extends Specification with Mockito {
+
+    val context = new Context {
+        override val url = URL("http://www.example.com")
+        override val params = Map[String, String]("1" -> "A", "2" -> "B")
+    }
+
+    "The Context.withParams method" should {
+
+        "override the parameters in an existing context" in {
+            context.withParams( Map("2" -> "!", "3" -> "C" ) )
+                .params must_== Map( "1" -> "A", "2" -> "!", "3" -> "C" )
+
+            context.withParams( "2" -> "!", "3" -> "C" )
+                .params must_== Map( "1" -> "A", "2" -> "!", "3" -> "C" )
+        }
+    }
+}
