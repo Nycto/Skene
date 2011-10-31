4d92e41d8ccc01cc4035ccd6ff00526a4868772f
Nycto <James@RoundEights.com>
2011-10-30 17:35:46 -0700

    Updated handlers to implement HttpServlet

--- src/main/scala/org/skene/Handler.scala
+++ src/main/scala/org/skene/Handler.scala
@@ -1,5 +1,9 @@
 package org.skene
 
+import org.skene.context.JettyContext
+
+import javax.servlet.http.{HttpServlet,HttpServletRequest,HttpServletResponse}
+
 /**
  * Methods for quickly building small handlers
  */
@@ -15,11 +19,50 @@ object Handler {
 /**
  * Defines an object that can handle a request
  */
-trait Handler {
+trait Handler extends HttpServlet {
+
     /**
      * Handles the given request and returns the response data
      */
     def handle( context: Context ): Response
+
+    /**
+     * The primary handler for using a handler has a servlet
+     */
+    private def doRequest (
+        request: HttpServletRequest, response: HttpServletResponse
+    ): Unit = {
+
+        // Set some sensible defaults
+        response.setContentType("text/html;charset=utf-8")
+
+        val result = handle( new JettyContext( request, response ) )
+
+        // Change the status code
+        response.setStatus( result.code.code )
+
+        // Add in any custom headers
+        result.headers.foreach { (header) =>
+            response.setHeader( header._1, header._2 )
+        };
+
+        result.content.render( response.getWriter )
+    }
+
+    /**
+     * @see HttpServlet
+     */
+    override def doGet (
+        request: HttpServletRequest, response: HttpServletResponse
+    ): Unit = doRequest( request, response )
+
+    /**
+     * @see HttpServlet
+     */
+    override def doPost (
+        request: HttpServletRequest, response: HttpServletResponse
+    ): Unit = doRequest( request, response )
+
 }
 
 /**
@@ -29,6 +72,7 @@ class CallbackHandler
     ( private val callback: (Context) => Response )
     extends Handler
 {
+
     /**
      * An alternate constructor that allows thunks to be used as handlers
      */
@@ -40,5 +84,7 @@ class CallbackHandler
      * @see Handler
      */
     override def handle( context: Context ) = callback(context)
+
 }
+
 
--- src/main/scala/org/skene/Server.scala
+++ src/main/scala/org/skene/Server.scala
@@ -1,13 +1,8 @@
 package org.skene
 
-import javax.servlet.http.{HttpServletRequest => ServletRequest}
-import javax.servlet.http.{HttpServletResponse => ServletResponse}
-import javax.servlet.ServletException
-
-import org.eclipse.jetty.server.handler.AbstractHandler
-import org.eclipse.jetty.server.handler.ContextHandler
 import org.eclipse.jetty.server.{Server => JettyServer}
-import org.eclipse.jetty.server.{Request => RawJettyRequest}
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 import org.skene.context._
 
@@ -17,9 +12,13 @@ import org.skene.context._
 object Server {
 
     def apply ( port: Int, handler: Handler ): Unit = {
+
         val server = new JettyServer(port)
-        server.setHandler( new JettyAdapter(handler) )
         server.setSendServerVersion(false)
+
+        val contextHandler = new ServletContextHandler( server, "/" )
+        contextHandler.addServlet(new ServletHolder(handler), "/")
+
         server.start();
         server.join();
     }
@@ -29,38 +28,6 @@ object Server {
 
     def apply ( port: Int, handler: (Context) => Response ): Unit
         = apply( port, Handler(handler) )
-}
 
-/**
- * Handles the hand-off between Servlet requests and the
- * custom request handler interface
- */
-private class JettyAdapter ( private val handler: Handler )
-    extends AbstractHandler {
-
-    override def handle(
-        target: String,
-        baseRequest: RawJettyRequest,
-        request: ServletRequest,
-        response: ServletResponse
-    ): Unit = {
-
-        // Set some sensible defaults
-        response.setContentType("text/html;charset=utf-8")
-
-        val result = handler.handle( new JettyContext( request, response ) )
-
-        // Change the status code
-        response.setStatus( result.code.code )
-
-        // Add in any custom headers
-        result.headers.foreach { (header) =>
-            response.setHeader( header._1, header._2 )
-        };
-
-        result.content.render( response.getWriter )
-
-        baseRequest.setHandled(true)
-    }
 }
 
