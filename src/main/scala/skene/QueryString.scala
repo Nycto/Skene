c52409cee76e5591ab51b317a621f9a0615a01cd
Nycto <James@RoundEights.com>
2013-06-02 20:52:09 -0700

    Add QueryString

---
+++ src/main/scala/skene/QueryString.scala
@@ -0,0 +1,71 @@
+package com.roundeights.skene
+
+import java.net.URLEncoder
+import java.net.URLDecoder
+
+/** Companion */
+object QueryString {
+
+    /** Create a query string from a list of tuples */
+    def apply ( params: (String, String)* ): QueryString
+        = new QueryString( params.toList )
+
+    /** Parses a string */
+    def apply ( queryString: String ): QueryString = {
+        def decode( value: String ) = URLDecoder.decode( value, "UTF8" )
+        queryString.split('&').foldRight( QueryString() )( (part, accum) => {
+            part.split("=", 2) match {
+                case Array(key) if key != ""
+                    => (decode(key) -> "") :: accum
+                case Array(key, value) if key != "" || value != ""
+                    => (decode(key) -> decode(value)) :: accum
+                case _ => accum
+            }
+        })
+    }
+
+}
+
+/**
+ * Represents a URL or POST query string
+ */
+case class QueryString (
+    override val toList: List[(String, String)] = Nil
+) extends Seq[(String, String)] {
+
+    /** {@inheritDoc} */
+    override def iterator = toList.iterator
+
+    /** {@inheritDoc} */
+    override def apply ( index: Int ) = toList(index)
+
+    /** {@inheritDoc} */
+    override def length = toList.length
+
+    /** Adds a parameter */
+    def :: ( param: (String, String) ) = QueryString(param :: toList)
+
+    /** {@inheritDoc} */
+    override def toString: String = {
+        toList.map( param => "%s=%s".format(
+            URLEncoder.encode( param._1, "UTF8" ),
+            URLEncoder.encode( param._2, "UTF8" )
+        ) ).mkString("&")
+    }
+
+    /** Groups a parameter by key for parameters that appear multiple times */
+    def grouped: Map[String, Seq[String]] = {
+        toList.foldRight( Map[String, Seq[String]]() ) {
+            (pair, accum) => accum + (
+                pair._1 -> ( pair._2 +: accum.get(pair._1).getOrElse(Nil) )
+            )
+        }
+    }
+
+    /** Returns the keys for this query string */
+    def keys: Set[String] = toList.foldLeft( Set[String]() )( _ + _._1 )
+
+    /** Returns the values in this query string */
+    def values: Set[String] = toList.foldLeft( Set[String]() )( _ + _._2 )
+}
+
---
+++ src/test/scala/skene/QueryStringTest.scala
@@ -0,0 +1,59 @@
+package test.scala.com.skene
+
+import org.specs2.mutable._
+
+import com.roundeights.skene.QueryString
+
+class QueryStringTest extends Specification {
+
+    "A QueryString" should {
+
+        "Allow extra parameters to be added" in {
+            ("a" -> "b") :: ("c" -> "d") :: QueryString() must_==
+                QueryString( "a" -> "b", "c" -> "d" )
+        }
+
+        "Generate URL encoded strings" in {
+            QueryString().toString must_== ""
+            QueryString( "a" -> "b", "c" -> "d" ).toString must_== "a=b&c=d"
+            QueryString( "a&b" -> "c=d" ).toString must_== "a%26b=c%3Dd"
+        }
+
+        "Convert to a Map" in {
+            QueryString().toMap must_== Map()
+            QueryString( "a" -> "b", "c" -> "d" ).toMap must_==
+                Map( "a" -> "b", "c" -> "d" )
+        }
+
+        "Group the same key if it appears multiple times" in {
+            QueryString(
+                "a" -> "1", "b" -> "2", "a" -> "3", "a" -> "3"
+            ).grouped must_== Map(
+                "a" -> Seq("1", "3", "3"), "b" -> Seq("2")
+            )
+        }
+
+        "Return a list of keys" in {
+            QueryString().keys must_== Set()
+            QueryString( "a" -> "1", "b" -> "2" ).keys must_== Set("a", "b")
+        }
+
+        "Return a list of values" in {
+            QueryString().values must_== Set()
+            QueryString( "a" -> "1", "b" -> "2" ).values must_== Set("1", "2")
+        }
+
+        "Parse a string" in {
+            QueryString("") must_== QueryString()
+            QueryString("a=b") must_== QueryString("a" -> "b")
+            QueryString("a=b&c=d") must_== QueryString("a" -> "b", "c" -> "d")
+            QueryString("a=&=d") must_== QueryString("a" -> "", "" -> "d")
+            QueryString("a==b") must_== QueryString("a" -> "=b")
+            QueryString("a&&b") must_== QueryString("a" -> "", "b" -> "")
+            QueryString("=&&=") must_== QueryString()
+            QueryString("a%26b=c%3Dd") must_== QueryString( "a&b" -> "c=d" )
+        }
+    }
+
+}
+
