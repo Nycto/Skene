ed8479000e91aa9e84f338d4fde75dcd56b3fe42
Nycto <James@RoundEights.com>
2013-10-10 08:21:40 -0700

    Fix and refine fluent interface

--- src/main/scala/skene/Fluent.scala
+++ src/main/scala/skene/Fluent.scala
@@ -2,9 +2,77 @@ package com.roundeights.skene
 
 import scala.annotation.tailrec
 import com.roundeights.skene._
+import com.roundeights.skene.Request.Method
 import scala.concurrent.ExecutionContext
 import java.util.concurrent.atomic.AtomicReference
 
+
+/** Attaches a matcher to a handler */
+class Finalize (
+    private[skene] val matcher: Matcher,
+    private val done:( Matcher, Handler ) => Unit
+) (
+    implicit private val ctx: ExecutionContext
+) {
+
+    /** Joins this matcher with a Handler */
+    def apply ( handler: Handler ): Unit = done( matcher, handler )
+
+    /** Joins this matcher with a Handler */
+    def apply ( callback: (Recover, Request, Response) => Unit ): Unit
+        = apply( Handler(callback) )
+
+    /** Joins this matcher with a Handler */
+    def apply ( callback: (Request, Response) => Unit ): Unit
+        = apply( Handler(callback) )
+
+    /** Joins this matcher with a Handler */
+    def apply ( callback: (Response) => Unit ): Unit
+        = apply( Handler(callback) )
+
+    /** {@inheritDoc} */
+    override def toString = matcher.toString
+
+    /**
+     * Creates a fluent interface that will combine this matcher with
+     * another expression
+     */
+    private def combiner(
+        combine: (Matcher, Matcher) => Matcher
+    ): Fluent = {
+        new Fluent {
+            override implicit protected val context = ctx
+            override def when ( rightMatcher: Matcher ): Finalize
+                = new Finalize( combine(matcher, rightMatcher), done )(ctx)
+        }
+    }
+
+    /**
+     * Builds a new Fluent matcher that requires this matcher and another
+     * to both pass
+     */
+    def and ( other: Finalize ): Finalize
+        = new Finalize( Matcher.and(matcher, other.matcher), done )
+
+    /**
+     * Builds a new Fluent matcher that requires this matcher and another
+     * to both pass
+     */
+    def and: Fluent = combiner( Matcher.and(_, _) )
+
+    /**
+     * Builds a new Fluent matcher that will match when either this
+     * or another matcher pass
+     */
+    def or ( other: Finalize ): Finalize
+        = new Finalize(Matcher.or(matcher, other.matcher), done)
+
+    /**
+     * Builds a new Fluent matcher that will match when either this
+     * or another matcher pass
+     */
+    def or: Fluent = combiner( Matcher.or(_, _) )
+}
 
 /**
  * A interface for fluently building a matcher
@@ -15,152 +83,96 @@ trait Fluent {
     implicit protected def context: ExecutionContext
 
     /** Associates a matcher with a handler */
-    def when ( matcher: Matcher )( handler: Handler ): Unit
-
-    /** Attaches a matcher to a handler */
-    class Finalize ( private[Fluent] val matcher: Matcher ) {
-
-        def apply ( callback: (Recover, Request, Response) => Unit ): Unit
-            = when( matcher )( Handler(callback) )
-
-        def apply ( callback: (Request, Response) => Unit ): Unit
-            = when( matcher )( Handler(callback) )
-
-        def apply ( callback: (Response) => Unit ): Unit
-            = when( matcher )( Handler(callback) )
-
-        def apply ( handler: Handler ): Unit
-            = when( matcher )( handler )
-
-        /**
-         * Creates a fluent interface that will combine this matcher with
-         * another expression
-         */
-        private def combiner(
-            combine: (Matcher, Matcher) => Matcher
-        ): Fluent = {
-            val outerFluent = Fluent.this
-            val outerCtx = Fluent.this.context
-            val outerMatch = matcher
-            new Fluent {
-                override implicit protected val context = outerCtx
-                override def when (matcher: Matcher)(handler: Handler): Unit
-                    = outerFluent.when( combine(outerMatch, matcher) )(handler)
-            }
-        }
-
-        /**
-         * Builds a new Fluent matcher that requires this matcher and another
-         * to both pass
-         */
-        def and ( other: Finalize ): Finalize
-            = new Finalize( Matcher.and(matcher, other.matcher) )
-
-        /**
-         * Builds a new Fluent matcher that requires this matcher and another
-         * to both pass
-         */
-        def and: Fluent = combiner( Matcher.and(_, _) )
-
-        /**
-         * Builds a new Fluent matcher that will match when either this
-         * or another matcher pass
-         */
-        def or ( other: Finalize ): Finalize
-            = new Finalize( Matcher.or(matcher, other.matcher) )
-
-        /**
-         * Builds a new Fluent matcher that will match when either this
-         * or another matcher pass
-         */
-        def or: Fluent = combiner( Matcher.or(_, _) )
-    }
+    def when ( matcher: Matcher ): Finalize
 
 
     /** Adds a handler that matches all requests */
-    def all: Finalize = new Finalize( Matcher.always )
+    def all: Finalize = when( Matcher.always )
 
     /** Adds a handler for any request matching the given path */
     def request ( path: String ): Finalize
-        = new Finalize( Matcher.path(path) )
+        = when( Matcher.path(path) )
 
     /** A helper method for building method specific handlers */
-    def method ( method: Request.Method ): Finalize
-        = new Finalize( Matcher.method( method ) )
+    def method ( method: Method ): Finalize
+        = when( Matcher.method( method ) )
 
     /** Applies a handler for GET requests */
-    lazy val isGet: Finalize = method( Request.Method.GET() )
+    lazy val isGet: Finalize = method( Method.GET() )
 
     /** Applies a handler for POST requests */
-    lazy val isPost: Finalize = method( Request.Method.POST() )
+    lazy val isPost: Finalize = method( Method.POST() )
 
     /** Applies a handler for DELETE requests */
-    lazy val isDelete: Finalize = method( Request.Method.DELETE() )
+    lazy val isDelete: Finalize = method( Method.DELETE() )
 
     /** Applies a handler for PUT requests */
-    lazy val isPut: Finalize = method( Request.Method.PUT() )
+    lazy val isPut: Finalize = method( Method.PUT() )
 
     /** Applies a handler for PATCH requests */
-    lazy val isPatch: Finalize = method( Request.Method.PATCH() )
+    lazy val isPatch: Finalize = method( Method.PATCH() )
 
     /** Adds a handler for GET requests to the given path */
-    def get ( path: String ): Finalize = isGet and request(path)
+    def get ( path: String ): Finalize
+        = when(Matcher.and(Matcher.method(Method.GET()), Matcher.path(path)))
 
     /** Adds a handler for POST requests to the given path */
-    def post ( path: String ): Finalize = isPost and request(path)
+    def post ( path: String ): Finalize
+        = when(Matcher.and(Matcher.method(Method.POST()), Matcher.path(path)))
 
     /** Adds a handler for DELETE requests to the given path */
-    def delete ( path: String ): Finalize = isDelete and request(path)
+    def delete ( path: String ): Finalize
+        = when(Matcher.and(Matcher.method(Method.DELETE()), Matcher.path(path)))
 
     /** Adds a handler for PUT requests to the given path */
-    def put ( path: String ): Finalize = isPut and request(path)
+    def put ( path: String ): Finalize
+        = when(Matcher.and(Matcher.method(Method.PUT()), Matcher.path(path)))
 
     /** Adds a handler for PATCH requests to the given path */
-    def patch ( path: String ): Finalize = isPatch and request(path)
+    def patch ( path: String ): Finalize
+        = when(Matcher.and(Matcher.method(Method.PATCH()), Matcher.path(path)))
 
     /** Adds a handler that matches secure requests */
-    lazy val isSecure: Finalize = new Finalize( Matcher.isSecure )
+    lazy val isSecure: Finalize = when( Matcher.isSecure )
 
     /** Adds a handler that matches non-secure requests */
-    lazy val notSecure: Finalize = new Finalize( Matcher.notSecure )
+    lazy val notSecure: Finalize = when( Matcher.notSecure )
 
     /** Adds a handler when the request has the given subdomain */
     def subdomain( subdomain: String ): Finalize
-        = new Finalize( Matcher.subdomain(subdomain) )
+        = when( Matcher.subdomain(subdomain) )
 
     /** Adds a handler when the request has a given host name */
-    def host( hostname: String ): Finalize
-        = new Finalize( Matcher.host(hostname) )
+    def host( hostname: String ): Finalize = when( Matcher.host(hostname) )
 
     /** Adds a handler when the request does not have a subdomain */
-    lazy val noSubdomain: Finalize = new Finalize( Matcher.noSubdomain )
+    lazy val noSubdomain: Finalize = when( Matcher.noSubdomain )
 
     /** Sets up a handler for the root directory */
     lazy val index: Finalize = request("/")
 
     /** Uses a custom callback as a matcher */
     def when ( callback: Request => Boolean ): Finalize = {
-        new Finalize( Matcher.call { request =>
+        when( Matcher.call { request =>
             Matcher.Result( callback(request) )
         } )
     }
 
     /** Uses a custom thunk as a matcher */
     def when ( callback: => Boolean ): Finalize
-        = new Finalize( Matcher.call { Matcher.Result( callback ) } )
+        = when( Matcher.call { Matcher.Result( callback ) } )
 
     /** Inverts the results of another matcher */
-    def not( other: Finalize ): Finalize
-        = new Finalize( Matcher.not(other.matcher) )
+    def not( other: Finalize ): Finalize = when( Matcher.not(other.matcher) )
 
     /** Inverts the results of another matcher */
     def not: Fluent = {
-        val outerFluent = Fluent.this
-        val outerCtx = Fluent.this.context
+        val outerFluent = this
+        val outerCtx = context
         new Fluent {
             override implicit protected val context = outerCtx
-            override def when (matcher: Matcher)(handler: Handler): Unit
-                = outerFluent.when( Matcher.not(matcher) )(handler)
+            override def when (matcher: Matcher): Finalize
+                = outerFluent.when( Matcher.not(matcher) )
         }
     }
 }
@@ -216,9 +228,12 @@ class Skene (
     override def matches ( request: Request ): Matcher.Result
         = dispatcher.get.matches( request )
 
-    /** Adds a new matcher */
-    override def when ( matcher: Matcher )( handler: Handler ): Unit
-        = dispatcher.set( _.add( matcher, handler ) )
+    /** {@inheritDoc} */
+    override def when ( matcher: Matcher ): Finalize = new Finalize(
+        matcher,
+        ( matcher: Matcher, handler: Handler ) =>
+            dispatcher.set( _.add( matcher, handler ) )
+    )
 
     /** Sets up a default handler */
     def default ( handler: Handler ): Unit
